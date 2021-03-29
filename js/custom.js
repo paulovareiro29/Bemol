7541ba129f9a876f3e697259a000452f1c9f5c85
BuildTools <unconfigured@null.spigotmc.org>
2021-03-29 13:47:15 +0100

    Navbar done

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,56 @@
-/* 
+//  HEADER Toggle Menu
+$(".header__navbar__menu").animate(
+  {
+    height: "hide",
+  },
+  0
+);
 
-1. Add your custom JavaScript code below
-2. Place the this code in your template:
+$(".header__navbar__trigger").click((e) => {
+  $(".header__navbar__menu").animate(
+    {
+      height: "toggle",
+    },
+    $("window").css("--transition-time")
+  );
 
-  
+  $(".wic-overlay").toggleClass("wic-overlay--active");
+});
 
-*/
+//  HEADER Dropdowns
+$(".header__navbar__menu__dropdown__toggle").next().animate(
+  {
+    height: "hide",
+  },
+  0
+);
+
+let dropDowns = $(".header__navbar__menu__dropdown__toggle").click((e) => {
+  let transitionTime = $(":root").css("--transition-time");
+
+  let menuItems = $(".header__navbar__menu__dropdown__toggle");
+  $.each(menuItems, (key, elem) => {
+    if (elem != e.target) {
+      $(elem).parent().removeClass("header__navbar__menu__dropdown--active");
+      $(elem)
+        .next()
+        .animate(
+          {
+            height: "hide",
+          },
+          $(window).width() < 768 ? transitionTime : 0
+        );
+    }
+  });
+
+  $(e.target).parent().toggleClass("header__navbar__menu__dropdown--active");
+  $(e.target)
+    .next()
+    .animate(
+      {
+        height: "toggle",
+        display: "flex",
+      },
+      $(window).width() < 768 ? transitionTime : 0
+    );
+});
